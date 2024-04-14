8f250a5a887187d605d25eaac87903ea0ef842a4
pantokratorius <eitu@mail.ru>
2024-04-14 14:11:13 +0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <!-- meta -->
   <meta charset="UTF-8" />
   <title>Марафон</title>
   <meta name="description" content="Марафон" />
   <meta name="keywords" content="Марафон" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-
 </head>
 
 <body>
-
+{{  'test' }}
 @for($i=1; $i < 14; $i++)
 
     <img src="images/{{ $i }}.jpg" />
